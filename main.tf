27e77184039ae42a644b3670cf8370b35a87a84c
Raksit <raksit.m@ku.th>
2020-05-09 16:40:25 +0700

    created storage account and container for testing databricks

--- main.tf
+++ main.tf
@@ -13,3 +13,16 @@ resource "azurerm_databricks_workspace" "databricks-connect-test" {
     Environment = "Development"
   }
 }
+
+resource "azurerm_storage_account" "storage-account-test" {
+  account_replication_type = "GRS"
+  account_tier             = "Standard"
+  location                 = azurerm_resource_group.rg-databricks.location
+  name                     = "databricksconnecttest"
+  resource_group_name      = azurerm_resource_group.rg-databricks.name
+}
+
+resource "azurerm_storage_container" "storage-container-test" {
+  name                 = "radio"
+  storage_account_name = azurerm_storage_account.storage-account-test.name
+}
